39c9e91501c613e937b9bf3ce9c526bdff276439
Rafael Inoue <rafael.niyasue@senacsp.edu.br>
2023-09-01 17:13:25 -0300

    01/09/2023

--- models/Usuario.php
+++ models/Usuario.php
@@ -18,6 +18,18 @@ class Usuario
     */
     public function buscar($id)
     {
+        try{
+            $query = ("SELECT * FROM {$this->table} WHERE id_usuario = $id");
+            $stmt = $this->db->prepare($query);  
+        }catch(PDOException $e ){
+            echo 'Erro na inserção: ' . $e->getMessage(); 
+        }
+
+        try{
+            $stmt->execute();
+        }catch(PDOException $e){
+            echo 'Erro ao buscar o usuário: ' . $e->getMessage();
+        }
     }
 
     /**
@@ -25,6 +37,18 @@ class Usuario
      */
     public function listar()
     {
+        try{
+            $query = ("SELECT * FROM {$this->table}"); 
+            $stmt = $this->db->prepare($query); 
+        }catch(PDOException $e){
+            echo 'Erro na inserção: ' . $e->getMessage(); 
+        }
+
+        try{
+            $stmt->execute();
+        }catch(PDOException $e){
+            echo 'Erro ao listar os alunos : ' . $e->getMessage(); 
+        }
     }
 
     /**
@@ -34,8 +58,25 @@ class Usuario
      */
     public function cadastrar($dados)
     {
-    }
+        try{
+            $query = "INSERT INTO {$this->table} (nome, email, senha, perfil) VALUES(:nome, :email, :senha, :perfil)";
+            $stmt = $this->db->prepare($query);  
+        }catch(PDOException $e){
+            echo 'Erro na preparação da consulta: ' . $e->getMessage(); 
+        }
 
+        $stmt->bindParam(':nome', $dados['nome']); 
+        $stmt->bindParam(':email', $dados['email']); 
+        $stmt->bindParam(':senha', $dados['senha']); 
+        $stmt->bindParam(':perfil', $dados['perfil']); 
+
+        try{
+            $stmt->execute(); 
+            echo 'Inserção bem-sucedida!'; 
+        }catch(PDOException $e){
+            echo 'Erro na inserção: ' . $e->getMessage(); 
+        }
+    }
 
     /**
      * Editar Usuário 
@@ -45,6 +86,24 @@ class Usuario
      */
     public function editar($id, $dados)
     {
+        try{
+            $query = ("UPDATE {$this->table} SET nome = :nome, email = :email, senha = :senha WHERE id_usuario = :$id");
+            $stmt = $this->db->prepare($query); 
+        }catch(PDOException $e){
+            echo 'Erro na preparação da consulta: ' . $e->getMessage(); 
+        }
+
+        $stmt->bindParam(':nome', $dados['nome']);
+        $stmt->bindParam(':email', $dados['email']);
+        $stmt->bindParam(':senha', $dados['senha']);
+        $stmt->bindParam(':perfil', $dados['perfil']);
+
+        try{
+            $stmt->execute();
+            echo 'Seus dados foram atualizados com sucesso!'; 
+        }catch(PDOException $e){
+            echo 'Erro na inserção: ' . $e->getMessage(); 
+        }
     }
 
     //Excluir Usuário
